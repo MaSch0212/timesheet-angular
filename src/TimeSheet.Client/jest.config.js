6b889bade9ce7aeba4e5360434b81fa71af472aa
Marc Schmidt <masch212@gmail.com>
2023-02-12 15:39:56 +0100

    chore: remove jest

--- src/TimeSheet.Client/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'time-sheet',
-    preset: '../../jest.config.js',
-    coverageDirectory: '../../coverage/apps/time-sheet/',
-    snapshotSerializers: [
-        'jest-preset-angular/AngularSnapshotSerializer.js',
-        'jest-preset-angular/HTMLCommentSerializer.js'
-    ]
-};
